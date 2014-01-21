2c6258b31f9c7b3c108b4e059b8711aa692ecf41
emetchar <emetchar@gmail.com>
2014-01-20 16:41:23 -0800

    added a makefile for partb

--- 1/partb/550server.cpp
+++ 1/partb/550server.cpp
@@ -5,13 +5,19 @@
   Phrazy Pingyang He
 */
 
+#include <cstdlib>
 #include <iostream> // print
 #include <fstream>  // file input
 #include <pthread.h>// pthreads
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 typedef struct ThreadData {
     // file name and connection socket go here
     // gets passed to readFile
+	char *fileName;
+	
 } ThreadData;
 
 void readFile(void* arg) {
@@ -20,6 +26,18 @@ void readFile(void* arg) {
 }
 
 int main(int argc, char** argv) {
+	if (argc != 3) {
+        // fail, not enough args
+
+        std::cerr << "Must have exactly 2 arguments." << std::endl;
+        std::cerr << "Usage:" << std::endl;
+        std::cerr << argv[0] << " <ipv4 address of server> <listening port>" << std::endl;
+        std::cerr << "ex: ./" << argv[0] <<  " 127.0.0.1 9000" << std::endl;
+
+		exit(0);
+	}
+
+
     // make a threadpool
 
     // open socket to listen

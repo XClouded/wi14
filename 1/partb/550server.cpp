e76639bac874f9544e13b24ee3ce1c0a52674e94
emetchar <emetchar@gmail.com>
2014-01-20 22:33:26 -0800

    a bit of work

--- 1/partb/550server.cpp
+++ 1/partb/550server.cpp
@@ -9,15 +9,20 @@
 #include <iostream> // print
 #include <fstream>  // file input
 #include <pthread.h>// pthreads
+#include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+
+using std::cerr;
+using std::cout;
+using std::endl;
 
 typedef struct ThreadData {
     // file name and connection socket go here
     // gets passed to readFile
 	char *fileName;
-	
+    int sock;
 } ThreadData;
 
 void readFile(void* arg) {
@@ -26,18 +31,24 @@ void readFile(void* arg) {
 }
 
 int main(int argc, char** argv) {
-	if (argc != 3) {
-        // fail, not enough args
+    int fd;
 
-        std::cerr << "Must have exactly 2 arguments." << std::endl;
-        std::cerr << "Usage:" << std::endl;
-        std::cerr << argv[0] << " <ipv4 address of server> <listening port>" << std::endl;
-        std::cerr << "ex: ./" << argv[0] <<  " 127.0.0.1 9000" << std::endl;
+    // check for correct # of args
+    if (argc != 3) {
+        cerr << "Must have exactly 2 arguments." << endl;
+        cerr << "Usage:" << endl;
+        cerr << argv[0] << " <ipv4 address of server> <listening port>" << endl;
+        cerr << "ex: " << argv[0] <<  " 127.0.0.1 9000" << endl;
 
 		exit(0);
 	}
 
+    // create a new socket for the server
+    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        cerr << "cannot create socket" << endl;
 
+        exit(0);
+    }
     // make a threadpool
 
     // open socket to listen

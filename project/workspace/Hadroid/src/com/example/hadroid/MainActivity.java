23594e02d8c2b9e06732ac7551a93aab401d04c1
emetchar <emetchar@gmail.com>
2014-03-02 23:25:58 -0800

    android client skeleton

--- project/workspace/Hadroid/src/com/example/hadroid/MainActivity.java
+++ project/workspace/Hadroid/src/com/example/hadroid/MainActivity.java
@@ -1,23 +1,35 @@
 package com.example.hadroid;
 
+import com.hadroid.service.HadroidService;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	// Start the  service
+	public void startHadroidService(View view) {
+		startService(new Intent(this, HadroidService.class));
+	}
+	// Stop the  service
+	public void stopHadroidService(View view) {
+		stopService(new Intent(this, HadroidService.class));
+	}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}    
 }
---
+++ project/workspace/Hadroid/src/com/hadroid/service/HadroidService.java
@@ -0,0 +1,46 @@
+package com.hadroid.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Main entry point into the Hadroid service
+ * @author jacobs22
+ *
+ */
+public class HadroidService extends Service {
+
+	public HadroidService() {
+		//TODO anything?
+	}
+
+	@Override
+	public IBinder onBind(Intent i) {
+		Log.d("HadroidService", "onBind");
+		Bundle args = i.getExtras();
+		//TODO do something
+		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		Log.d("HadroidService", "onCreate");
+		Toast.makeText(this, " HadroidService created", Toast.LENGTH_LONG).show();
+	}
+
+	@Override
+	public void onStart(Intent intent, int startId) {
+		Log.d("HadroidService", "onStart");
+		Toast.makeText(this, "HadroidService started", Toast.LENGTH_LONG).show();
+	}
+
+	@Override
+	public void onDestroy() {
+		Log.d("HadroidService", "onDestroy");
+		Toast.makeText(this, "HadroidService destroyed", Toast.LENGTH_LONG).show();
+	}
+}

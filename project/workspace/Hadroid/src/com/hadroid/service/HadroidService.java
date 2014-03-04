875b0eafcae1974fb711bf66fd39ae740f718c35
emetchar <emetchar@gmail.com>
2014-03-03 16:49:00 -0800

    work on service

--- project/workspace/Hadroid/src/com/hadroid/service/HadroidService.java
+++ project/workspace/Hadroid/src/com/hadroid/service/HadroidService.java
@@ -1,6 +1,7 @@
 package com.hadroid.service;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
@@ -8,9 +9,16 @@ import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
+
+import uw.edu.hadroid.workflow.HadroidFunction;
+
+import dalvik.system.DexClassLoader;
+import dalvik.system.DexFile;
 
 import message.HadroidMessage;
 import message.RequestTaskMessage;
+import message.TaskMessage;
 import android.app.Service;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -30,12 +38,13 @@ public class HadroidService extends Service {
 	//private static final String SERVER_IP = "172.28.7.96";
 	private static final String SERVER_IP = "169.254.33.128";
 	private static final int SERVER_PORT = 6669;
-	
-	private final File tmpDir;
+
+	private final File dexDir;
+	private File tmpFile;
 	private Socket serverSocket;
 
 	public HadroidService() {
-		tmpDir = this.getCacheDir();
+		dexDir = this.getApplicationContext().getDir("dex", 0);
 		serverSocket = null;
 	}
 
@@ -57,6 +66,15 @@ public class HadroidService extends Service {
 	public void onStart(Intent intent, int startId) {
 		Log.d(LOG_TAG, "onStart");
 		Toast.makeText(this, "HadroidService started", Toast.LENGTH_LONG).show();
+		
+		try {
+			tmpFile = this.getCacheDir().createTempFile("tmp", "jar");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Log.e(LOG_TAG, "IOException when making temp jar");
+		}
+		
 		Worker w = new Worker();
 		w.execute();
 	}
@@ -65,7 +83,7 @@ public class HadroidService extends Service {
 	public void onDestroy() {
 		Log.d(LOG_TAG, "onDestroy");
 		Toast.makeText(this, "HadroidService destroyed", Toast.LENGTH_LONG).show();
-		
+
 		if(serverSocket != null) {
 			try {
 				serverSocket.close();
@@ -75,7 +93,8 @@ public class HadroidService extends Service {
 			serverSocket = null;
 		}
 	}
-	
+
+	// this does all of the actual work!
 	class Worker extends AsyncTask<Void, Void, Void> {
 
 		@Override
@@ -85,22 +104,50 @@ public class HadroidService extends Service {
 				Log.d(LOG_TAG, "worker executing...");
 				serverSocket = new Socket(SERVER_IP, SERVER_PORT);
 				serverSocket.setTcpNoDelay(true);
-				
+
 				// create a request message
 				HadroidMessage msg = new RequestTaskMessage();
-				
+
 				// send the message
 				OutputStream outstream = serverSocket.getOutputStream(); 
 				ObjectOutputStream oos = new ObjectOutputStream(outstream);
 				oos.writeObject(new RequestTaskMessage());
 				Log.d(LOG_TAG, "message sent...");
-				
+
 				// get a response
 				InputStream in = serverSocket.getInputStream();
-	            ObjectInputStream ois = new ObjectInputStream(in);
-	            msg = (HadroidMessage) ois.readObject();
-	            Log.d(LOG_TAG, "message received: " + msg);
-				
+				ObjectInputStream ois = new ObjectInputStream(in);
+				msg = (HadroidMessage) ois.readObject();
+				Log.d(LOG_TAG, "message received: " + msg);
+
+
+				if (msg instanceof TaskMessage) {
+					Log.d(LOG_TAG, "TaskMessage received");
+					TaskMessage tm = (TaskMessage)msg;
+					
+					// write dex to disk
+					FileOutputStream out = new FileOutputStream(tmpFile);
+					out.write(tm.getTask().getDexFile());
+					out.close();
+					
+					// load in the dex file
+					DexClassLoader classLoader = new DexClassLoader(tmpFile.getPath(),
+							dexDir.getPath(), null, getClass().getClassLoader());
+					DexFile df = DexFile.loadDex(tmpFile.getAbsolutePath(),
+							new File(dexDir, tmpFile.getName() + ".odex").getAbsolutePath(), 0);
+					for (Enumeration<String> iter = df.entries(); iter.hasMoreElements();) {
+		                String className = iter.nextElement();
+		                Log.i(LOG_TAG,"Found class: " + className);
+		                Class<?> cls = Class.forName(className, true, classLoader);
+		                if (HadroidFunction.class.isAssignableFrom(cls)) {
+		                    // HadroidFunction class found
+		                    Log.i(LOG_TAG, "Found class: " + className);
+		                    //TODO instantiate the runnable thing
+		                    //blades.add((HadroidFunction) cls.newInstance());
+		                }
+		            }
+				}				
+
 			} catch (UnknownHostException e) {
 				Log.e(LOG_TAG, "UnknownHostException");
 				e.printStackTrace();
@@ -121,9 +168,9 @@ public class HadroidService extends Service {
 					serverSocket = null;
 				}
 			}
-			
+
 			return null;
 		}
-		
+
 	}
 }
